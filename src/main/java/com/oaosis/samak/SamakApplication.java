9ae522f81dc46802793229a2d4aa5bebd633a818
chaeyoungeee <chaeinge@gmail.com>
2026-02-09 21:45:44 +0900

    init

---
+++ src/main/java/com/oaosis/samak/SamakApplication.java
@@ -0,0 +1,13 @@
+package com.oaosis.samak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oaosis/samak/SamakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oaosis.samak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
