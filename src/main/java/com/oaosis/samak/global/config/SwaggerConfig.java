ecb90b2efcc3c13c99c0c8b9c508a38bbfbad691
chaeyoungeee <chaeinge@gmail.com>
2026-02-10 23:09:07 +0900

    feat: swagger 설정

---
+++ src/main/java/com/oaosis/samak/global/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.oaosis.samak.global.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SAMAK API",
+                version = "v1.0.0",
+                description = "SAMAK API 명세서입니다."
+        ),
+        servers = {
+                @Server(url = "https://samak.mooo.com", description = "Production Server"),
+                @Server(url = "http://localhost:8080", description = "Local Server")
+        }
+)
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "JWT";
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(SECURITY_SCHEME_NAME);
+        Components components = new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                        .name(SECURITY_SCHEME_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat(SECURITY_SCHEME_NAME));
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
