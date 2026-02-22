a091c3c30718fa37a7b5f021500b16cc178b3e24
chaeyoungeee <chaeinge@gmail.com>
2026-02-22 23:17:36 +0900

    fix: swagger에서 이미지 파일이 string으로 잘못 요청되는 버그 해결

--- src/main/java/com/oaosis/samak/infra/s3/controller/ImageController.java
+++ src/main/java/com/oaosis/samak/infra/s3/controller/ImageController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,14 +24,14 @@ public class ImageController {
 
     @Operation(summary = "단일 이미지 업로드", description = "이미지 파일을 multipart/form-data 형식으로 전송해주시면 됩니다.")
     @PostMapping(consumes = "multipart/form-data")
-    public ResponseEntity<ApiResponse<String>> uploadImage(MultipartFile multipartFile) {
+    public ResponseEntity<ApiResponse<String>> uploadImage(@RequestPart("multipartFile") MultipartFile multipartFile) {
         return ResponseEntity.ok(
                 ApiResponse.success(imageService.uploadImage(multipartFile)));
     }
 
     @Operation(summary = "다중 이미지 업로드", description = "이미지 파일들을 multipart/form-data 형식으로 전송해주시면 됩니다.")
     @PostMapping(path = "/multiple", consumes = "multipart/form-data")
-    public ResponseEntity<ApiResponse<List<String>>> uploadImages(List<MultipartFile> multipartFiles) {
+    public ResponseEntity<ApiResponse<List<String>>> uploadImages(@RequestPart("multipartFiles") List<MultipartFile> multipartFiles) {
         return ResponseEntity.ok(
                 ApiResponse.success(imageService.uploadImages(multipartFiles)));
     }
