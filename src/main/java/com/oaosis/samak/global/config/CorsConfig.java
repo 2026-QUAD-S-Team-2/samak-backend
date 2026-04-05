96a6b366e62171ce26ebe5d689fb7e11ec0d9db4
chaeyoungeee <chaeinge@gmail.com>
2026-04-05 09:57:11 +0900

    feat: Cors 설정 추가

---
+++ src/main/java/com/oaosis/samak/global/config/CorsConfig.java
@@ -0,0 +1,39 @@
+package com.oaosis.samak.global.config;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CorsConfig implements WebMvcConfigurer {
+    private static  final String CLIENT = "";
+    private static final String SERVER = "samak.mooo";
+
+    public static CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5555",
+                "http://localhost:8080",
+                "https://" + CLIENT,
+                "https://" + SERVER
+        ));
+
+        configuration.setAllowedMethods(List.of("HEAD", "POST", "GET", "DELETE", "PUT", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of(HttpHeaders.LOCATION, HttpHeaders.SET_COOKIE));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
